e329ea6a3e95ba8ab824f3fcec3c614dcb2e8004
Martti Laine <martti@marttilaine.com>
2016-05-24 00:14:12 +0300

    Initial commit

---
+++ readme.md
@@ -0,0 +1,46 @@
+# The Router
+
+Promise-powered routing independent of view framework.
+
+
+## Usage
+
+Illustrative example:
+
+```
+const router = new Router();
+
+const ensurePersonIsLoaded = params => {
+    return getPerson(params.name)
+        .then(person => state.person = person);
+};
+
+router.setRoutes({
+    '/': HomeView,
+    '/person/:name': {
+        before: ensurePersonIsLoaded,
+        '/': params => PersonView,
+        '/edit': params => EditPersonView
+    }
+});
+
+router.setAfterParse(view => {
+    viewEngine.render(view);
+});
+
+router.listen();
+```
+
+In this example `viewEngine` receives a view from the route handlers. Route handlers are only ran after any `before`-hooks have been ran.
+
+See [examples/basic.html](#) for a more elaborate version.
+
+
+## Credits
+
+Powered by [route-parser](https://github.com/rcs/route-parser) and [history](https://github.com/mjackson/history).
+
+
+## License
+
+MIT
---
+++ test/index.js
@@ -0,0 +1,229 @@
+'use strict';
+
+const Promise = require('bluebird');
+const test = require('tape');
+const Router = require('../');
+
+const handlers = {};
+for(let i = 97; i <= 107; i++) {
+    const char = String.fromCharCode(i);
+    handlers[char] = () => char;
+}
+
+test('normalizePath', t => {
+    t.ok(Router.normalizePath, 'should exist');
+    t.equal(Router.normalizePath('/'), '/');
+    t.equal(Router.normalizePath('/foobar'), '/foobar');
+    t.equal(Router.normalizePath('/foo/bar'), '/foo/bar');
+    t.equal(Router.normalizePath('//'), '/');
+    t.equal(Router.normalizePath('//foobar'), '/foobar');
+    t.equal(Router.normalizePath('/foo//bar'), '/foo/bar');
+    t.equal(Router.normalizePath('//foo//bar'), '/foo/bar');
+    t.equal(Router.normalizePath('/foobar/'), '/foobar');
+    t.equal(Router.normalizePath('/foo/bar/'), '/foo/bar');
+    t.equal(Router.normalizePath('foobar'), '/foobar');
+    t.end();
+});
+
+test('flattenRouteHash', t => {
+    t.ok(Router.flattenRouteHash, 'should exist');
+
+    t.deepEqual(Router.flattenRouteHash({
+        '/': handlers.a
+    }), {
+        '/': { handler: handlers.a }
+    }, 'should support handlers');
+
+    t.deepEqual(Router.flattenRouteHash({
+        '/': handlers.a,
+        '/foobar': handlers.a
+    }), {
+        '/': { handler: handlers.a },
+        '/foobar': { handler: handlers.a }
+    }, 'should support equal handlers');
+
+    t.deepEqual(Router.flattenRouteHash({
+        '/': handlers.a,
+        '/foobar': handlers.b
+    }), {
+        '/': { handler: handlers.a },
+        '/foobar': { handler: handlers.b }
+    }, 'should support several handlers');
+
+    t.deepEqual(Router.flattenRouteHash({
+        '/': handlers.a,
+        '/foo': {
+            '/bar': handlers.b,
+            '/baz': handlers.c,
+            '/deep': {
+                '/': handlers.d,
+                '/when-will-this-end': handlers.e
+            }
+        }
+    }), {
+        '/': { handler: handlers.a },
+        '/foo/bar': { handler: handlers.b },
+        '/foo/baz': { handler: handlers.c },
+        '/foo/deep': { handler: handlers.d },
+        '/foo/deep/when-will-this-end': { handler: handlers.e }
+    }, 'should support nested handlers');
+
+    t.deepEqual(Router.flattenRouteHash({
+        '/': {
+            '/': handlers.a,
+            before: handlers.b
+        }
+    }), {
+        '/': { handler: handlers.a, before: [handlers.b] }
+    }, 'should pass hooks');
+
+    t.deepEqual(Router.flattenRouteHash({
+        '/': {
+            before: handlers.b,
+            '/': handlers.a
+        }
+    }), {
+        '/': { handler: handlers.a, before: [handlers.b] }
+    }, 'should pass hooks (order irrevelant)');
+
+    t.deepEqual(Router.flattenRouteHash({
+        '/': {
+            before: handlers.a,
+            '/': handlers.b,
+            '/foobar': handlers.c
+        }
+    }), {
+        '/': { handler: handlers.b, before: [handlers.a] },
+        '/foobar': { handler: handlers.c, before: [handlers.a] }
+    }, 'should inherit hooks');
+
+    t.deepEqual(Router.flattenRouteHash({
+        '/': {
+            before: handlers.a,
+            '/': handlers.b,
+            '/foo': {
+                '/bar': handlers.d,
+                '/baz': {
+                    before: handlers.e,
+                    '/': handlers.f
+                }
+            }
+        }
+    }), {
+        '/': { handler: handlers.b, before: [handlers.a] },
+        '/foo/bar': { handler: handlers.d, before: [handlers.a] },
+        '/foo/baz': { handler: handlers.f, before: [handlers.a, handlers.e] }
+    }, 'should inherit multiple hooks');
+
+    t.end();
+});
+
+test('should handle routes', t => {
+    const router = new Router();
+
+    const requests = [];
+
+    router.setRoutes({
+        '/': () => requests[requests.length] = 'home',
+        '/foo': () => requests[requests.length] = 'foo',
+        '/foobar': {
+            '/': () => requests[requests.length] = 'foobar',
+            '/:name': params => requests[requests.length] = `foobar/${params.name}`
+        }
+    });
+
+    return Promise.resolve()
+        .then(() => router.parseRoute('/'))
+        .then(() => router.parseRoute('/foo'))
+        .then(() => router.parseRoute('/foobar'))
+        .then(() => router.parseRoute('/foobar/phil'))
+        .then(() => {
+            t.deepEqual(requests, ['home', 'foo', 'foobar', 'foobar/phil']);
+            t.end();
+        });
+});
+
+test('should handle hooks', t => {
+    const router = new Router();
+
+    const requests = [];
+
+    router.setRoutes({
+        before: () => requests[requests.length] = 'before all',
+        '/': () => requests[requests.length] = 'home',
+        '/foo': () => requests[requests.length] = 'foo',
+        '/:name': {
+            before: params => requests[requests.length] = `before ${params.name}`,
+            '/': params => requests[requests.length] = `${params.name}`,
+            '/x': params => requests[requests.length] = `${params.name}/x`
+        }
+    });
+
+    return Promise.resolve()
+        .then(() => router.parseRoute('/'))
+        .then(() => router.parseRoute('/foo'))
+        .then(() => router.parseRoute('/phil'))
+        .then(() => router.parseRoute('/phil/x'))
+        .then(() => {
+            t.deepEqual(requests, [
+                'before all', 'home',
+                'before all', 'foo',
+                'before all', 'before phil', 'phil',
+                'before all', 'before phil', 'phil/x'
+            ]);
+            t.end();
+        });
+});
+
+test('should handle hooks (Promises)', t => {
+    const router = new Router();
+
+    const requests = [];
+
+    router.setRoutes({
+        before: () => Promise.delay(200).then(() => requests[requests.length] = 'before'),
+        '/:name': {
+            '/': params => requests[requests.length] = params.name,
+            '/foo': {
+                before: () => Promise.delay(200).then(() => requests[requests.length] = 'before foo'),
+                '/': params => requests[requests.length] = `${params.name}/foo`,
+            }
+        }
+    });
+
+    return Promise.resolve()
+        .then(() => router.parseRoute('/phil'))
+        .then(() => router.parseRoute('/phil/foo'))
+        .then(() => {
+            t.deepEqual(requests, [
+                'before', 'phil',
+                'before', 'before foo', 'phil/foo'
+            ]);
+            t.end();
+        });
+});
+
+test('should run afterParse after handler', t => {
+    const router = new Router();
+
+    const requests = [];
+    const afterParse = returnValueFromHandler => requests[requests.length] = returnValueFromHandler;
+
+    router.setRoutes({
+        '/': () => 'could return home view here',
+        '/:name': params => `name: ${params.name.toUpperCase()}`
+    });
+
+    router.setAfterParse(afterParse);
+
+    return Promise.resolve()
+        .then(() => router.parseRoute('/'))
+        .then(() => router.parseRoute('/phil'))
+        .then(() => {
+            t.deepEqual(requests, [
+                'could return home view here',
+                'name: PHIL'
+            ]);
+            t.end();
+        });
+});
